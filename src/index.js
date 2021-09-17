8b9a1d9f337ee77551d8a515ad8db9bebb809789
Horácio Ciufi Neto <repositorio@hospitalcasadesaude.com.br>
2021-09-17 08:55:30 -0300

    commit 2

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import http from 'http';
 const app = express();
 const server = http.Server(app);
 
+/***a rota é definida aqui através do "/" onde posso lançar qualquer endereço **/
 app.get('/',(req, res)=>{
     res.send('hello word');
 });
 
-server.listen('3333',()=>{
+server.listen('3333',()=>{ 
     console.log('servidor iniciado porta',3333)}
     );
